a5b5d2e3334e0a3620629f3e4673469aaab83333
sdijhegwkh <Student>
2025-04-02 11:29:10 +0700

    Layout 1 phan

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="header">
+        <h1>Header</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="menu">
+        <a href="#">Link 1</a><br />
+        <a href="#">Link 2</a><br />
+        <a href="#">Link 3</a>
+      </div>
+      <div className="content">
+        <h3>Data Total Info</h3>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="footer">
+        <h4>DataTable</h4>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
