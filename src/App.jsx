469caeb132fea75f454a777c87b3385c3c2b79ad
sdijhegwkh <sprout890@gmail.com>
2025-04-06 16:10:01 +0700

    API overview

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import DataTotalInfo from './DataInfo';
+import Header from './Header';
 
 function App() {
   return (
     <div className="container">
       <div className="header">
-        <h1>Header</h1>
+        <Header/>
       </div>
       <div className="menu">
         <a href="#">Link 1</a><br />
@@ -13,9 +15,7 @@ function App() {
         <a href="#">Link 3</a>
       </div>
       <div className="content">
-        <h3>Data Total Info</h3>
-        <p>
-        </p>
+      <DataTotalInfo />
       </div>
       <div className="footer">
         <h4>DataTable</h4>
